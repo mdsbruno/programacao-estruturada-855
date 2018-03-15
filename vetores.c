0b611a4a092bc462d0e4fc5b001633b54a16720a
BRUNO SOUZA <2017102723@UC.ACAD>
2018-03-15 08:23:53 -0300

    vetores

---
+++ vetores.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define max 20
+
+int main(){
+/*	
+	//exercicio 1
+	int vetor[max];
+	int i;
+	
+	for(i=0; i<max; i++){
+		printf("digite um numero: ");
+		scanf("%f", &vetor[i]);
+		
+		printf("o numero %d digitado foi: %d", i, vetor[i] );
+	}
+	
+	system("pause");
+	
+	return 0;	
+/*	
+	//exercicio 2
+	int vet[15];
+	int i, soma;
+	soma = 0;
+	for(i=0; i<15; i++){
+		printf("Digite um numero: ");
+		scanf("%d", &vet[i]);
+		soma=soma+vet[i];		
+	}
+	
+	printf("Soma dos numeros: %d", soma);
+	system("pause");
+	return 0;
+/*	
+//exercicio 4	
+	int vet[10];
+	int i, menor, maior;
+
+	maior = 0;
+	for(i=0; i<10; i++){
+		printf("digite o numero: ");
+		scanf("%d", &vet[i]);
+	}
+	for(i=0;i<10; i++ ){
+		if (vet[i] > maior){
+			maior = vet[i];
+		} else if (vet[i] < menor){
+			menor = vet[i];
+		}
+	}
+	printf("o valor menor foi: %d", menor);
+	printf("o valor maior foi: %d", maior);
+	system("pause");
+	return 0;
+*/
+//exercicio 3
+
+	int vet[10];
+	int i;
+	
+	for(i=0; i<10; i++){
+		printf("digite um numero: ");
+		scanf("%d", &vet[i]);
+	}
+	for(i=0; i<10; i++){
+				
+		
+		
+		/*if (    ((vet[i] % vet[i]) = 1) or ((vet[i] % 1 ) = vet[i]) ){
+			printf("O numero %d é primo e sua posição é %d", vet[i], i);
+		}*/
+	}
+}
